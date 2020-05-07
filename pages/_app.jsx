aa7d8f53fdd0e9d36f59f01fb58e202cbf75e2d6
Foreverjie <864129545@qq.com>
2020-05-07 23:20:16 +0800

    debug

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,6 @@
 // import "../style/styles.css"
 // import "../style/article_editor.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-select/dist/css/bootstrap-select.min.css"
 import "../style/style2.css"
 
 // This default export is required in a new `pages/_app.js` file.
