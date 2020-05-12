e07742850cf5471bfdaf6d5cccfc72e590e82c3c
Foreverjie <864129545@qq.com>
2020-05-12 22:55:52 +0800

    update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// export const urlPrefix = "http://localhost:4000/api"
-export const urlPrefix = "https://www.jie1203.com/api"
+export const urlPrefix = "http://localhost:4000/api"
+// export const urlPrefix = "https://www.jie1203.com/api"
--- pages/article/[id].jsx
+++ pages/article/[id].jsx
@@ -1,17 +1,43 @@
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import { urlPrefix } from '../../config'
-
+import { useRouter } from "next/router"
+import axios from "axios"
+import { urlPrefix } from "../../config"
+import { Fragment } from "react"
+import IndexHeader from "../../components/index_header"
+import Nav from "../../layout/nav"
+import ReactMarkdown from "react-markdown"
+import { Badge } from "antd"
 
 const Article = (props) => {
   const router = useRouter()
   const { id } = router.query
 
   return (
-    <div>
-      <h1>Post: {id}</h1>
-      <h2>Article: {props.article.title}</h2>
-    </div>
+    <Fragment>
+      <IndexHeader title={props.article.title} />
+      <Nav />
+
+      <div
+        className="d-flex flex-column align-items-center"
+        style={{ marginTop: "10vh", height: "90vh" }}
+      >
+        <div
+          style={{
+            width: "fit-content",
+            maxWidth: "60%",
+            fontSize: "40px",
+            lineHeight: "48px",
+            fontFamily:
+              'medium-content-title-font, Georgia, Cambria, "Times New Roman", Times, serif',
+          }}
+        >
+          {props.article.title}
+        </div>
+        <h3 style={{ width: "fit-content", maxWidth: "60%" }}>
+          <Badge color={"blue"} text={props.article.author.name} />
+        </h3>
+        <ReactMarkdown source={props.article.content} />
+      </div>
+    </Fragment>
   )
 }
 
@@ -21,7 +47,7 @@ Article.getInitialProps = async (ctx) => {
   const res = await axios.get(`${urlPrefix}/articles/${id}`)
 
   const intialProps = {
-    article: res.data
+    article: res.data,
   }
   return intialProps
 }
